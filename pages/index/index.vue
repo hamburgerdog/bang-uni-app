07e087c5344c4ea4f4fa22befb98f5e6570a2d19
hamburgerdog <838980000@qq.com>
2021-05-26 22:22:03 +0800

    :tada:【初始化】帮帮小程序项目基础布局初始化 :memo:技术栈： 1.	uni-app + vue-simple-router + uni-ui 2.	scss + eslint ps:	项目使用hbuilderx中的uni_modules方式引入了uni-ui|eslint-vue|eslint-js，可能在使用npm|yarn安装时可能会出现依赖安装遗漏的问题！！！！！请自行添加！！！

---
+++ pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view>
+		<uni-swiper-dot :info="info" :current="current" field="content" mode="default">
+			<swiper @change="change" class="swiper-box" :indicator-dots="false" :autoplay="true" :interval="3000" :duration="1000">
+				<swiper-item>
+					<view class="swiper-item">
+						<image src="../../static/icon/home-select.png" mode=""></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item">
+						<image src="../../static/icon/index-select.png" mode=""></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item">
+						<image src="../../static/icon/setting-select.png" mode=""></image>
+					</view>
+				</swiper-item>
+			</swiper>
+		</uni-swiper-dot>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				info: [{
+						content: '内容A'
+					},
+					{
+						content: '内容B'
+					},
+					{
+						content: '内容C'
+					}
+				],
+				current: 0,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			change(e) {
+				this.current = e.detail.current;
+			}
+		},
+		components: {}
+	}
+</script>
+
+<style lang="scss">
+</style>
