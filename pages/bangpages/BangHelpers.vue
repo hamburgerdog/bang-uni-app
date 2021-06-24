641a727c51dd27aecda0e52b41c328485046b70d
hamburgerdog <838980000@qq.com>
2021-06-24 19:36:04 +0800

    :zap:【合并】修改帮帮展示页面筛选数据选项

--- pages/bangpages/BangHelpers.vue
+++ pages/bangpages/BangHelpers.vue
@@ -7,13 +7,18 @@
 		<cl-filter-bar @change="onChange" class="filter-bar">
 			<cl-filter-item label="酬金优先" />
 			<cl-filter-item label="时间优先" />
+			<cl-filter-item label="性别限定" v-model="gender" :options="genderList" type="dropdown" prop="rank"
+				theme="grid" />
 		</cl-filter-bar>
-		<uni-notice-bar scrollable="true" single="true" text="禁止发布代课|替考|刷单求助  , 禁止发布代课|替考|刷单求助 , 禁止发布代课|替考|刷单求助  , 禁止发布代课|替考|刷单求助" speed="80"
-			showIcon="true" />
-		<view class="card-box">
+		<uni-notice-bar scrollable="true" single="true"
+			text="禁止发布代课|替考|刷单求助  , 禁止发布代课|替考|刷单求助 , 禁止发布代课|替考|刷单求助  , 禁止发布代课|替考|刷单求助" speed="80" showIcon="true" />
+		<view class="card-box" v-if="visiable">
 			<view class="card" v-for="card in helpers" :key="card.index">
 				<MyCard :card="card"></MyCard>
 			</view>
+		</view>
+		<view class="card-box" v-else>
+			<p>页面加载中。。。</p>
 		</view>
 	</view>
 </template>
@@ -32,7 +37,7 @@
 						showFooter: true,
 						highLight: true,
 						footerText: '备注：脚注测试',
-						path:'/pages/detailpages/BangHelper'
+						path: '/pages/detailpages/BangHelper'
 					},
 					{
 						title: '测试Test',
@@ -44,7 +49,7 @@
 						showFooter: true,
 						highLight: true,
 						footerText: '备注：脚注测试',
-						path:'/pages/detailpages/BangHelper'
+						path: '/pages/detailpages/BangHelper'
 					},
 					{
 						title: '测试Test',
@@ -56,7 +61,7 @@
 						showFooter: true,
 						highLight: false,
 						footerText: '备注：脚注测试',
-						path:'/pages/detailpages/BangHelper'
+						path: '/pages/detailpages/BangHelper'
 					},
 					{
 						title: '测试Test',
@@ -68,9 +73,24 @@
 						showFooter: false,
 						highLight: false,
 						footerText: '备注：脚注测试',
-						path:'/pages/detailpages/BangHelper'
+						path: '/pages/detailpages/BangHelper'
 					}
-				]
+				],
+				genderList: [{
+						label: '女生',
+						value: '女生'
+					},
+					{
+						label: '男生',
+						value: '男生'
+					},
+					{
+						label: '不限',
+						value: '不限'
+					}
+				],
+				gender: '',
+				visiable: true
 			}
 		},
 		methods: {
@@ -83,10 +103,10 @@
 </script>
 
 <style lang="scss" scoped>
-	.card-box{
+	.card-box {
 		padding: 0 20rpx;
-		
-		.card{
+
+		.card {
 			position: relative;
 			margin-bottom: 20rpx;
 		}
